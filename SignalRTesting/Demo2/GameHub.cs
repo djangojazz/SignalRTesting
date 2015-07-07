f17fa14de6e1ed66a11522dab1ae279d8af9974f
djangojazz <brettmorin2@gmail.com>
2015-07-07 13:05:11 -0700

    Made SignalR work

---
+++ SignalRTesting/Demo2/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+
+namespace Demo2
+{
+    public class GameHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello(DateTime.Now.ToString());
+        }
+    }
+}
---
+++ SignalRTesting/Demo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Demo2.Startup))]
+namespace Demo2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
