a9476bca75c0e4ed4ec439e13278de7bc1d68097
djangojazz <brettmorin2@gmail.com>
2015-07-13 14:12:07 -0700

    Added two more projects

---
+++ SignalRTesting/SignalR.Sample/StockTickerHub.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+
+namespace SignalR.Sample
+{
+    [HubName("stockTicker")]
+    public class StockTickerHub : Hub
+    {
+        private readonly global::SignalR.Sample.StockTicker _stockTicker;
+
+        public StockTickerHub() :
+            this(StockTicker.Instance)
+        {
+
+        }
+
+        public StockTickerHub(global::SignalR.Sample.StockTicker stockTicker)
+        {
+            _stockTicker = stockTicker;
+        }
+
+        public IEnumerable<Stock> GetAllStocks()
+        {
+            return _stockTicker.GetAllStocks();
+        }
+
+        public string GetMarketState()
+        {
+            return _stockTicker.MarketState.ToString();
+        }
+
+        public void OpenMarket()
+        {
+            _stockTicker.OpenMarket();
+        }
+
+        public void CloseMarket()
+        {
+            _stockTicker.CloseMarket();
+        }
+
+        public void Reset()
+        {
+            _stockTicker.Reset();
+        }
+    }
+}
